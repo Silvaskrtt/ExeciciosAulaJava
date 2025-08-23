86c4baa1021fc0932ed8545f329ef96ed0fadfb5
Silvaskrtt <murilodasilvadaschagas12@gmail.com>
2025-08-22 21:08:26 -0300

    feat: exercicio 3

--- src/exerc3/ContaBancaria.java
+++ src/exerc3/ContaBancaria.java
@@ -1,4 +1,56 @@
 package exerc3;
 
 public class ContaBancaria {
+
+    String titular;
+    double saldo;
+    double saque;
+    double depositar;
+
+    public ContaBancaria(String titular, double saldo, double saque, double depositar) {
+        this.titular = titular;
+        this.saldo = saldo;
+        this.saque = saque;
+        this.depositar = depositar;
+    }
+
+    public ContaBancaria (){
+        // metodo vazio
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getSaque() {
+        return saque;
+    }
+
+    public void setSaque(double saque) {
+        if (saldo > 0.0) {
+            this.saque = saque;
+        } else {
+            System.out.println("Saque invãlido, não há saldo na conta!");
+        }
+    }
+
+    public double getDepositar() {
+        return depositar;
+    }
+
+    public void setDepositar(double depositar) {
+        this.depositar = depositar;
+    }
 }
--- src/exerc3/ContaBancariaTest.java
+++ src/exerc3/ContaBancariaTest.java
@@ -1,4 +1,11 @@
 package exerc3;
 
-public class ContaBacariaTest {
+public class ContaBancariaTest {
+
+    public static void Main(String[] args) {
+
+        System.out.println("----------- Bem-vindo ao Banco do Senai -----------");
+
+
+    }
 }

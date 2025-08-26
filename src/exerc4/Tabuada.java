6b8711cec4eb7a5daa4ac8b5f9cc9056177a6aa7
Murilo Chagas <154563139+Silvaskrtt@users.noreply.github.com>
2025-08-26 10:27:25 -0300

    feat: exercicio 4 finalizado

--- src/exerc4/Tabuada.java
+++ src/exerc4/Tabuada.java
@@ -2,21 +2,31 @@ package exerc4;
 
 public class Tabuada {
 
-    private int numero;
+    private int numerosInt;
+    private double numerosDouble;
 
-    public Tabuada(int numero) {
-        this.numero = numero;
+    public Tabuada(int numeroInt, double numerosDouble) {
+        this.numerosInt = numeroInt;
+        this.numerosDouble = numerosDouble;
     }
 
     public Tabuada () {
 
     }
 
-    public int getNumero() {
-        return numero;
+    public int getNumerosInt() {
+        return numerosInt;
     }
 
-    public void setNumero(int numero) {
-        this.numero = numero;
+    public void setNumerosInt(int numerosInt) {
+        this.numerosInt = numerosInt;
+    }
+
+    public double getNumerosDouble() {
+        return numerosDouble;
+    }
+
+    public void setNumerosDouble(double numerosDouble) {
+        this.numerosDouble = numerosDouble;
     }
 }
--- src/exerc4/TabuadaTest.java
+++ src/exerc4/TabuadaTest.java
@@ -11,29 +11,57 @@ public class TabuadaTest {
             Tabuada minhaTabuada = new Tabuada();
 
             String operador;
-            int tamVet, pos, calculo;
+            int tamVet, pos, calculoInt = 0;
+            double calculoDouble = 0;
 
-            System.out.println("--------- Bem-vindo a Tabuada ---------");
+            System.out.println("--------- Bem-vindo à Tabuada ---------");
 
             System.out.print(""" 
-                    Digite uma operação que deseje saber a tabuada -> | + | - | ÷ | x |.
-                    Digite aqui o simbólo da operação: \s""");
+                    Digite uma operação que deseje saber a tabuada -> | + | - | ÷ | x |
+                    Digite aqui o símbolo da operação: \s""");
             operador = input.nextLine();
 
             System.out.print("Digite até qual número a tabuada deve ser calculada: ");
             tamVet = input.nextInt();
 
-            System.out.print("Digite qual número deseja saber a tabuada: ");
-            int theNumero = input.nextInt();
-            minhaTabuada.setNumero(theNumero);
+            // declarada fora para usar depois
+            int theNumerosInt = 0;
+            double theNumerosDouble = 0;
+
+            if (operador.equals("+") || operador.equals("-") || operador.equals("x")) {
+                System.out.print("Digite qual número deseja saber a tabuada: ");
+                theNumerosInt = input.nextInt();
+                minhaTabuada.setNumerosInt(theNumerosInt);
+            } else if (operador.equals("÷")) {
+                System.out.print("Digite qual número deseja saber a tabuada: ");
+                theNumerosDouble = input.nextDouble();
+                minhaTabuada.setNumerosDouble(theNumerosDouble);
+            }
 
             System.out.println("\n---- Resultado da Tabuada ----");
 
             if (operador.equals("+")) {
-                for (pos = 0; pos < tamVet; pos++) {
-                    calculo = theNumero + pos;
-                    System.out.println(theNumero + "+" + tamVet + "=" + calculo);
+                for (pos = 1; pos <= tamVet; pos++) {
+                    calculoInt = theNumerosInt + pos;
+                    System.out.println(theNumerosInt + " + " + pos + " = " + calculoInt);
                 }
+            } else if (operador.equals("-")) {
+                for (pos = 1; pos <= tamVet; pos++) {
+                    calculoInt = theNumerosInt - pos;
+                    System.out.println(theNumerosInt + " - " + pos + " = " + calculoInt);
+                }
+            } else if (operador.equals("x")) {
+                for (pos = 1; pos <= tamVet; pos++) {
+                    calculoInt = theNumerosInt * pos;
+                    System.out.println(theNumerosInt + " x " + pos + " = " + calculoInt);
+                }
+            } else if (operador.equals("÷")) {
+                for (pos = 1; pos <= tamVet; pos++) {
+                    calculoDouble = theNumerosDouble / pos;
+                    System.out.printf("%.2f ÷ %d = %.2f%n", theNumerosDouble, pos, calculoDouble);
+                }
+            } else {
+                System.out.println("Operador inválido!");
             }
         }
     }

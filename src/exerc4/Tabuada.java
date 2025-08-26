d89619a465fc7ef205cd436523677b710a35828d
Murilo Chagas <154563139+Silvaskrtt@users.noreply.github.com>
2025-08-26 10:26:43 -0300

    feat: exercicio 4

--- src/exerc4/Tabuada.java
+++ src/exerc4/Tabuada.java
@@ -1,4 +1,22 @@
 package exerc4;
 
 public class Tabuada {
+
+    private int numero;
+
+    public Tabuada(int numero) {
+        this.numero = numero;
+    }
+
+    public Tabuada () {
+
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
 }
--- src/exerc4/TabuadaTest.java
+++ src/exerc4/TabuadaTest.java
@@ -1,4 +1,40 @@
 package exerc4;
 
+import java.util.Scanner;
+
 public class TabuadaTest {
+
+    public static void main(String[] args) {
+
+        try (Scanner input = new Scanner(System.in)) {
+
+            Tabuada minhaTabuada = new Tabuada();
+
+            String operador;
+            int tamVet, pos, calculo;
+
+            System.out.println("--------- Bem-vindo a Tabuada ---------");
+
+            System.out.print(""" 
+                    Digite uma operação que deseje saber a tabuada -> | + | - | ÷ | x |.
+                    Digite aqui o simbólo da operação: \s""");
+            operador = input.nextLine();
+
+            System.out.print("Digite até qual número a tabuada deve ser calculada: ");
+            tamVet = input.nextInt();
+
+            System.out.print("Digite qual número deseja saber a tabuada: ");
+            int theNumero = input.nextInt();
+            minhaTabuada.setNumero(theNumero);
+
+            System.out.println("\n---- Resultado da Tabuada ----");
+
+            if (operador.equals("+")) {
+                for (pos = 0; pos < tamVet; pos++) {
+                    calculo = theNumero + pos;
+                    System.out.println(theNumero + "+" + tamVet + "=" + calculo);
+                }
+            }
+        }
+    }
 }

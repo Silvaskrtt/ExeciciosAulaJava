a6b6afa91de2da139a027e330f66bba20bca5ae5
Murilo Chagas <154563139+Silvaskrtt@users.noreply.github.com>
2025-08-26 22:13:35 -0300

    feat: exercio 5 concluido

--- src/exerc5/PessoaIMC.java
+++ src/exerc5/PessoaIMC.java
@@ -1,4 +1,42 @@
 package exerc5;
 
-public class Pessoa {
+public class PessoaIMC {
+
+    private String nome;
+    private double peso;
+    private double altura;
+
+    public PessoaIMC(String nome, double peso, double altura) {
+        this.nome = nome;
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    public PessoaIMC() {
+        // metodo vazio
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
 }
--- src/exerc5/PessoaIMCTest.java
+++ src/exerc5/PessoaIMCTest.java
@@ -1,4 +1,52 @@
 package exerc5;
 
+import java.util.Scanner;
+
 public class PessoaIMCTest {
+
+    public static double calcIMC(double thePeso, double theAltura) {
+        double resultado;
+        resultado = thePeso / (theAltura * theAltura);
+        return resultado;
+    }
+
+    public static void main(String[] args) {
+
+        try (Scanner input = new Scanner(System.in)) {
+
+            PessoaIMC meuIMC = new PessoaIMC();
+
+            // variáveis do programa
+            double theAltura = 0;
+            double thePeso = 0;
+            String theNome;
+
+            // Mensagem de boas-vindas ao usuário
+            System.out.println("------ Bem-vindo(a) a calculadora de IMC ------");
+
+            System.out.print("Qual o seu nome: ");
+            theNome = input.nextLine();
+            meuIMC.setNome(theNome);
+
+            System.out.print("Por favor digite sua altura:");
+            theAltura = input.nextDouble();
+            meuIMC.setAltura(theAltura);
+
+            System.out.print("Por favor, digite seu peso:");
+            thePeso = input.nextDouble();
+            meuIMC.setPeso(thePeso);
+
+            double resultado = calcIMC(thePeso, theAltura);
+
+            if (resultado >=30) {
+                System.out.printf("Calculo do IMC: %.2f você está com Obesidade.", resultado);
+            } else if (resultado >= 25.0  && resultado < 29.9) {
+                System.out.printf("Calculo do IMC: %.2f você está com Sobrepeso.", resultado);
+            } else if (resultado >= 18.5 && resultado < 24.9) {
+                System.out.printf("Calculo do IMC: %.2f você está com Peso normal.", resultado);
+            } else {
+                System.out.printf("Calculo do IMC: %.2f você está com Magreza.", resultado);
+            }
+        }
+    }
 }

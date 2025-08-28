f0ca0bb8c6c0a2281f998bc9f17b42a490800cd2
Murilo Chagas <154563139+Silvaskrtt@users.noreply.github.com>
2025-08-27 21:26:19 -0300

    feat: exercicio 8

---
+++ src/Exerc8/Carro.java
@@ -0,0 +1,29 @@
+package Exerc8;
+
+public class Carro {
+    private int velocidade;
+
+    public Carro() {
+        this.velocidade = 0; // começa parado
+    }
+
+    public int getVelocidade() {
+        return velocidade;
+    }
+
+    public void acelerar(int incremento) {
+        if (velocidade + incremento <= 180) {
+            velocidade += incremento;
+        } else {
+            velocidade = 180; // trava no limite
+        }
+    }
+
+    public void frear(int decremento) {
+        if (velocidade - decremento >= 0) {
+            velocidade -= decremento;
+        } else {
+            velocidade = 0; // trava em 0
+        }
+    }
+}
---
+++ src/Exerc8/CarroTest.java
@@ -0,0 +1,29 @@
+package Exerc8;
+
+public class CarroTest {
+    public static void main(String[] args) throws InterruptedException {
+
+        Carro meuCarro = new Carro();
+
+        System.out.println("Iniciando aceleração...");
+
+        // Acelera de 10 em 10 até 180
+        while (meuCarro.getVelocidade() < 180) {
+            meuCarro.acelerar(10);
+            System.out.println("Velocidade: " + meuCarro.getVelocidade() + " km/h");
+            Thread.sleep(200); // espera 200ms pra mostrar a subida
+        }
+
+        System.out.println("Velocidade máxima atingida!");
+
+        // Agora começa a frear
+        System.out.println("\nIniciando frenagem...");
+        while (meuCarro.getVelocidade() > 0) {
+            meuCarro.frear(15);
+            System.out.println("Velocidade: " + meuCarro.getVelocidade() + " km/h");
+            Thread.sleep(200); // espera 200ms pra mostrar a descida
+        }
+
+        System.out.println("O carro parou!");
+    }
+}
